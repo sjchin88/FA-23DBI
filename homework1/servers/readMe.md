e718ad2d32d25aeeba90630321d5c0baa7827446
danielyhbi <105143607+danielyhbi@users.noreply.github.com>
2023-10-08 11:52:00 -0700

    upload servers

---
+++ homework1/servers/readMe.md
@@ -0,0 +1,9 @@
+# Servers
+
+it contains two servers
+- Go
+- Tomcat (Java)
+
+They are both server stubs that checks inputs and return a fix data.
+- Album id has to be an integer
+- url has to be valid
---
+++ homework1/servers/tomcat server/src/main/java/org/danielyhbi/cs6650/data/Album.java
@@ -0,0 +1,13 @@
+package org.danielyhbi.cs6650.data;
+
+public final class Album {
+    private final String title;
+    private final String artist;
+    private final int year;
+
+    public Album(String title, String artist, int year) {
+        this.title = title;
+        this.artist = artist;
+        this.year = year;
+    }
+}
